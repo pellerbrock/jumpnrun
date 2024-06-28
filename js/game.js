045a8f3515bd860448dbd5b3d21bf012197c7d2e
pellerbrock <philellerbrock@web.de>
2024-06-28 13:56:20 +0200

    added a platform

--- js/game.js
+++ js/game.js
@@ -4,7 +4,6 @@ const ctx = canvas.getContext('2d');
 let heroImage = new Image();
 heroImage.src = 'assets/hero.png';
 
-
 let hero = {
     x: 50,
     y: canvas.height - 100,
@@ -16,8 +15,21 @@ let hero = {
     jumping: false
 };
 
+// Plattform-Daten
+let platform = {
+    x: 200,
+    y: canvas.height - 150,
+    width: 100,
+    height: 20
+};
+
 function drawHero() {
     ctx.drawImage(heroImage, hero.x, hero.y, hero.width, hero.height);
+}
+
+function drawPlatform() {
+    ctx.fillStyle = '#654321'; // Braune Farbe für die Plattform
+    ctx.fillRect(platform.x, platform.y, platform.width, platform.height);
 }
 
 function clear() {
@@ -34,6 +46,16 @@ function update() {
     } else {
         hero.dy = 0;
         hero.jumping = false;
+    }
+
+    // Plattform-Kollisionsprüfung
+    if (hero.y + hero.height >= platform.y &&
+        hero.y + hero.height <= platform.y + platform.height &&
+        hero.x + hero.width > platform.x &&
+        hero.x < platform.x + platform.width) {
+        hero.dy = 0;
+        hero.jumping = false;
+        hero.y = platform.y - hero.height; // Auf die Plattform setzen
     }
 }
 
@@ -61,6 +83,7 @@ function jump() {
 function gameLoop() {
     clear();
     drawHero();
+    drawPlatform(); // Plattform zeichnen
     update();
     requestAnimationFrame(gameLoop);
 }
