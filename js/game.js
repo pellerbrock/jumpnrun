cbb155b002d6f3149c4663ec96dffe2bda6caeb9
pellerbrock <philellerbrock@web.de>
2024-06-28 19:28:20 +0200

    final change!

--- js/game.js
+++ js/game.js
@@ -18,9 +18,9 @@ let hero = {
 
 let platforms = [
     { x: 0, y: canvas.height - 60, width: 800, height: 20 },
-    { x: 350, y: canvas.height - 100, width: 100, height: 10 },
-    { x: 550, y: canvas.height - 130, width: 60, height: 10 },
-    { x: 200, y: canvas.height - 100, width: 60, height: 10 }
+    { x: 350, y: canvas.height - 170, width: 100, height: 10 },
+    { x: 550, y: canvas.height - 150, width: 60, height: 10 },
+    { x: 200, y: canvas.height - 130, width: 60, height: 10 }
 ];
 
 let goal = {
