4a4cd9b844913f8559dff459f824f49d2dc3b183
pellerbrock <philellerbrock@web.de>
2024-06-28 19:14:12 +0200

    cc

--- js/game.js
+++ js/game.js
@@ -17,7 +17,7 @@ let hero = {
 };
 
 let platforms = [
-    { x: 0, y: canvas.height - 100, width: 100, height: 20 },
+    { x: 0, y: canvas.height - 80, width: 800, height: 20 },
     { x: 350, y: canvas.height - 200, width: 100, height: 20 }
 ];
 
